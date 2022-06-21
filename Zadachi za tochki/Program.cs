5837256af059a8c46a6ad5a6178f753467b89ad1
Rev0ld <103751290+Rev0ld@users.noreply.github.com>
2022-06-21 13:51:52 +0300

    Update Program.cs

--- Zadachi za tochki/Program.cs
+++ Zadachi za tochki/Program.cs
@@ -32,6 +32,30 @@ namespace Zadachi_za_tochki
             //POLAR TO DECART
             //y^2 = x^2 + r^2 - 2r*x
 
+        public static double Distance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        static double CalculateTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+
+            double side1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double side2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            double side3 = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+
+            return side1 + side2 + side3;
+        }
+        static void Triugulnik()
+        {
+
+            int[] coordinates = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Console.WriteLine("{0:F2}", CalculateTriangle(coordinates[0], coordinates[1], coordinates[2], coordinates[3], coordinates[4], coordinates[5]));
+        }
+    }
+
+}
 
 
 
